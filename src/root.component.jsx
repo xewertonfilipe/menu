6e128c0071ad2fa5836db5484fc5218e881375cd
Ewerton Filipe <xewertonfilipe@gmail.com>
2026-07-14 14:04:30 -0300

    feat(refactor): reorganize Root component

--- src/root.component.jsx
+++ src/root.component.jsx
@@ -1,11 +1,6 @@
 import { Sidebar } from "./components/Sidebar";
 
 export default function Root(props) {
-  // TODO: validar
-  function onClick() {
-    const event = new CustomEvent("onNavClick");
-    document.dispatchEvent(event);
-  }
   return (
     <>
       <Sidebar />
