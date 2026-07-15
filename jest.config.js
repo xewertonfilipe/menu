9d7bf007a316829646d0a4ef5c0cb724d10762a4
Ewerton Filipe <xewertonfilipe@gmail.com>
2026-07-14 22:26:07 -0300

    feat(mocks): add mock implementation for single-spa navigation

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
     "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
+    "single-spa$": "<rootDir>/__mocks__/single-spa.js",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
